566735e8e5ac384ee1bccda62f323cb2926e6a40
unknown <agoloubna19@gmail.com>
2023-05-25 22:30:26 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__) #create a class 
+app.secret_key = "manbearpig_MUDMAN888"
+
+@app.route("/hello")
+def index():
+	flash("Quel est ton nom ?")
+	return render_template("index.html")
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Bonjour " + str(request.form['name_input']) + ", ravi de te voir !")
+	return render_template("index.html")
