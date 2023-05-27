309839e45caeb4e64492bd342c35f0dc29a68177
AgLoubna <123814823+AgLoubna@users.noreply.github.com>
2023-05-27 20:45:26 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__) #create a class 
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/hello")
+@app.route("/")
 def index():
 	flash("Quel est ton nom ?")
 	return render_template("index.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Bonjour " + str(request.form['name_input']) + ", ravi de te voir !")
-	return render_template("index.html")
+    if request.method == 'POST':
+        flash("Bonjour " + str(request.form['name_input']) + ", ravi de te voir !")
+    return render_template("index.html")
